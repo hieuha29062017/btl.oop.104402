27b1e441f4b80eab47cd88be2b14ec2c4c20578b
hieuha29062017 <sniper19121998@gmail.com>
2018-12-05 10:22:12 +0700

    Nguyen Viet Tien: Location Version_1

--- src/entity/Location.java
+++ src/entity/Location.java
@@ -1,17 +1,8 @@
 package entity;
 
 public class Location extends Entity {
-
-	public void setName(String vs){
-		this.name = vs;
-	}
-	
-	public void setDescription(String vs){
-		this.description = vs;
-	}
-	
-	public void setSource(Source arg){
-		this.source = arg;
+	public Location(String id, String name, String des, Source src){
+		super(id, name, des, src);
 	}
 	
 	public String getName(){
--- src/generate/GenerateLocation.java
+++ src/generate/GenerateLocation.java
@@ -1,88 +1,65 @@
 package generate;
 
-<<<<<<< HEAD
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
-
-
 import entity.*;
 
 public class GenerateLocation implements IGenerateData {
 
-		private static ArrayList<String> listLocationName = new ArrayList<String>();
-		private static ArrayList<String> listDescription = new ArrayList<String>();
-		private static ArrayList<Source> listSource = new ArrayList<Source>();
+	private static ArrayList<String> listLocationName = new ArrayList<String>();
+	private static ArrayList<String> listDescription = new ArrayList<String>();
+	private static ArrayList<Source> listSource = new ArrayList<Source>();
 		
-		public GenerateLocation() throws IOException{
-			BufferedReader inputStream = null;
+	public GenerateLocation() throws IOException{
+		BufferedReader inputStream = null;
 	
-			try{
-				inputStream = new BufferedReader(new FileReader("locationInfor.txt"));
+		try{
+			inputStream = new BufferedReader(new FileReader("locationInfor.txt"));
 				
-				String vs;
-				while((vs = inputStream.readLine()) != null && !vs.equals("-1")){
-					listLocationName.add(vs);
-				}
-				
-				while((vs = inputStream.readLine()) != null && !vs.equals("-2")){
-					listDescription.add(vs);
-				}
-				
-				while((vs = inputStream.readLine()) != null && !vs.equals("-3")){
-					Source temp = new Source(vs);
-					listSource.add(temp);
-				}
+			String vs;
+			while((vs = inputStream.readLine()) != null && !vs.equals("-1")){
+				listLocationName.add(vs);
 			}
-			finally{
-				if(inputStream != null)
-					inputStream.close();
+				
+			while((vs = inputStream.readLine()) != null && !vs.equals("-2")){
+				listDescription.add(vs);
+			}
+			
+			while((vs = inputStream.readLine()) != null && !vs.equals("-3")){
+				Source temp = new Source(vs);
+				listSource.add(temp);
 			}
 		}
-=======
-import entity.Entity;
-
-public class GenerateLocation implements IGenerateData {
-
-	public Entity generateData(int idNumber) {
-		return null;
-		// TODO Auto-generated method stub
->>>>>>> 8e2f8a84bff6cdd9756cf71054607df7ca458a50
-
-		public Entity generateData() {
-			Random rand = new Random();				
-			Location lct = new Location();
-			
-			int pos = rand.nextInt(listLocationName.size());
-			lct.setName(listLocationName.get(pos));
-			
-			pos = rand.nextInt(listDescription.size());
-			lct.setDescription(listDescription.get(pos));
-			
-			pos = rand.nextInt(listSource.size());
-			lct.setSource(listSource.get(pos));
-			
-			return lct;
-		
-		}
-		public static void main(String[] args) {
-			try{
-				GenerateLocation lct = new GenerateLocation();
-				Location pes = (Location)lct.generateData();
-				
-				System.out.println(pes.getName());
-				System.out.println(pes.getDes());
-				System.out.println(pes.getSource().getLink());
-				System.out.println(pes.getSource().getDate());
-			}
-			catch(IOException ex){
-				
-			}
+		finally{
+			if(inputStream != null)
+				inputStream.close();
 		}
 	}
+
+	public Entity generateData(int idNumber) {
+		Random rand = new Random();				
+			
+		int pos = rand.nextInt(listLocationName.size());
+		String name = listLocationName.get(pos);
+			
+		pos = rand.nextInt(listDescription.size());
+		String des = listDescription.get(pos);
+			
+		pos = rand.nextInt(listSource.size());
+		Source src = listSource.get(pos);
+		
+		String id = name.replaceAll(" ", "_");
+		id = id + "_" + Integer.toString(idNumber, 8);
+			
+		Location lct = new Location(id, name, des, src);
+		
+		return lct;	
+	}
+
+}
 
 
