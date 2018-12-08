306c9715b6c62070c43b9cb456684eea7ff883ca
TienQuanVu <quanvutien98@gmail.com>
2018-12-09 00:22:08 +0700

    Organization
    Organization

--- src/entity/Organization.java
+++ src/entity/Organization.java
@@ -1,5 +1,23 @@
 package entity;
 
 public class Organization extends Entity {
+	public Organization(String id, String name, String des, Source src){
+		super(id, name, des, src);
+	}
 
+	public String getID(){
+		return this.identify;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getDes(){
+		return this.description;
+	}
+	
+	public Source getSource(){
+		return this.source;
+	}
 }
