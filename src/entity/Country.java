0b2cbaba89e543fa3566a001c63b59d66366fc7b
Nguyen Viet Tien <viettien98htttbn@gmail.com>
2018-12-10 13:06:21 +0700

    lan 2

--- src/entity/Country.java
+++ src/entity/Country.java
@@ -1,5 +1,8 @@
 package entity;
 
 public class Country extends Entity {
+	public Country(String id, String name, String des, Source src){
+		super(id, name, des, src);
+	}
 
 }
--- src/generate/GenerateCountry.java
+++ src/generate/GenerateCountry.java
@@ -1,12 +1,61 @@
 package generate;
 
-import entity.Entity;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import entity.*;
 
 public class GenerateCountry implements IGenerateData {
-
+	
+	private static ArrayList<String> listCountryName = new ArrayList<String>();
+	private static ArrayList<String> listDescription = new ArrayList<String>();
+	private static ArrayList<Source> listSource = new ArrayList<Source>();
+	public GenerateCountry() throws IOException{
+		BufferedReader inputStream = null;
+	
+		try{
+			inputStream = new BufferedReader(new FileReader("locationInfor.txt"));
+				
+			String vs;
+			while((vs = inputStream.readLine()) != null && !vs.equals("-1")){
+				listCountryName.add(vs);
+			}
+				
+			while((vs = inputStream.readLine()) != null && !vs.equals("-2")){
+				listDescription.add(vs);
+			}
+			
+			while((vs = inputStream.readLine()) != null && !vs.equals("-3")){
+				Source temp = new Source(vs);
+				listSource.add(temp);
+			}
+		}
+		finally{
+			if(inputStream != null)
+				inputStream.close();
+		}
+	}
 	public Entity generateData(int idNumber) {
-		return null;
-		// TODO Auto-generated method stub
+		Random rand = new Random();				
+		
+		int pos = rand.nextInt(listCountryName.size());
+		String name = listCountryName.get(pos);
+			
+		pos = rand.nextInt(listDescription.size());
+		String des = listDescription.get(pos);
+			
+		pos = rand.nextInt(listSource.size());
+		Source src = listSource.get(pos);
+		
+		String id = name.replaceAll(" ", "_");
+		id = id + "_" + Integer.toString(idNumber, 8);
+			
+		Country ct = new Country(id, name, des, src);
+		
+		return ct;		
 
 	}
 }
