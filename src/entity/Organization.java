af18b3e1a7edd84bdfcf193c071ede1730fd0fd6
hieuha29062017 <sniper19121998@gmail.com>
2018-12-10 15:25:12 +0700

    Vu Tien Quan: change Organization

--- src/entity/Organization.java
+++ src/entity/Organization.java
@@ -4,20 +4,4 @@ public class Organization extends Entity {
 	public Organization(String id, String name, String des, Source src){
 		super(id, name, des, src);
 	}
-
-	public String getID(){
-		return this.identify;
-	}
-	
-	public String getName(){
-		return this.name;
-	}
-	
-	public String getDes(){
-		return this.description;
-	}
-	
-	public Source getSource(){
-		return this.source;
-	}
 }
