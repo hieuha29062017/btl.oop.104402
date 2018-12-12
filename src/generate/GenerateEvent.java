d377d1e9438e62d9735b7c09955ebdd8315e63b4
vnhoang1998 <45594695+vnhoang1998@users.noreply.github.com>
2018-12-12 08:00:50 +0700

    abc

--- src/generate/GenerateEvent.java
+++ src/generate/GenerateEvent.java
@@ -1,13 +1,63 @@
 package generate;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
-import entity.Entity;
+import entity.*;
 
 public class GenerateEvent implements IGenerateData {
 
-	public Entity generateData(int idNumber) {
-		return null;
-		// TODO Auto-generated method stub
-
+	private static ArrayList<String> listEventName = new ArrayList<String>();
+	private static ArrayList<String> listDescription = new ArrayList<String>();
+	private static ArrayList<Source> listSource = new ArrayList<Source>();
+		
+	public GenerateEvent() throws IOException{
+		BufferedReader inputStream = null;
+	
+		try{
+			inputStream = new BufferedReader(new FileReader("EventInfor.txt"));
+				
+			String vs;
+			while((vs = inputStream.readLine()) != null && !vs.equals("-1")){
+				listEventName.add(vs);
+			}
+				
+			while((vs = inputStream.readLine()) != null && !vs.equals("-2")){
+				listDescription.add(vs);
+			}
+			
+			while((vs = inputStream.readLine()) != null && !vs.equals("-3")){
+				Source temp = new Source(vs);
+				listSource.add(temp);
+			}
+		}
+		finally{
+			if(inputStream != null)
+				inputStream.close();
+		}
 	}
 
+	public Entity generateData(int idNumber) {
+		Random rand = new Random();				
+			
+		int pos = rand.nextInt(listEventName.size());
+		String name = listEventName.get(pos);
+			
+		pos = rand.nextInt(listDescription.size());
+		String des = listDescription.get(pos);
+			
+		pos = rand.nextInt(listSource.size());
+		Source src = listSource.get(pos);
+		
+		String id = name.replaceAll(" ", "_");
+		id = id + "_" + Integer.toString(idNumber, 8);
+			
+		Location lct = new Location(id, name, des, src);
+		
+		return lct;	
+	}
 }
+
+
